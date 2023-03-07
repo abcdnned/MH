ff90b87c6635950d95470f46c8730df009e3669e
tom <tom.yang01@sap.com>
2023-03-07 16:28:27 +0800

    udpate

--- ragdoll1.sh
+++ ragdoll1.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-cp ragdoll1.fbx ~/Documents/u3dprojects/PyhsicsSaber/Assets/Model/
+#cp ragdoll1.fbx ~/Documents/u3dprojects/PyhsicsSaber/Assets/Model/
+cp ragdoll1.fbx ~/Documents/u3dprojects/IKMan/Assets/
